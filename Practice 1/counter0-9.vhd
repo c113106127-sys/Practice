46fa155f606224d723a2eb34fe372fee00d66c6d
c113106127-sys <c113106127@nkust.edu.tw>
2025-11-24 22:18:09 +0800

    Refactor counter to use reset positive logic

--- Practice 1/counter0-9.vhd
+++ Practice 1/counter0-9.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 entity practice1 is
     port (
         clk      : in  std_logic;
-        rst_n    : in  std_logic;
+        rst_p    : in  std_logic; 
         en       : in  std_logic;
         up_cnt   : out std_logic_vector(3 downto 0);
         down_cnt : out std_logic_vector(3 downto 0)
@@ -13,32 +13,35 @@ entity practice1 is
 end entity;
 
 architecture rtl of practice1 is
-    constant MAX_VAL : unsigned(3 downto 0) := to_unsigned(9, 4);
-    constant MIN_VAL : unsigned(3 downto 0) := to_unsigned(0, 4);
+    
+   
+    signal up_reg    : integer := 0;
+    signal down_reg  : integer := 9;
 
-    signal up_reg    : unsigned(3 downto 0) := MIN_VAL;
-    signal down_reg  : unsigned(3 downto 0) := MAX_VAL;
 begin
 
-    process(clk, rst_n)
+   
+    process(clk, rst_p) 
     begin
-        if rst_n = '0' then
-            up_reg   <= MIN_VAL;
-            down_reg <= MAX_VAL;
+       
+        if rst_p = '1' then 
+            up_reg   <= 0;
+            down_reg <= 9;
 
+       
         elsif rising_edge(clk) then
             if en = '1' then
-                
-                -- Up counter
-                if up_reg = MAX_VAL then
-                    up_reg <= MIN_VAL;
+
+               
+                if up_reg = 9 then
+                    up_reg <= 0;
                 else
                     up_reg <= up_reg + 1;
                 end if;
 
-                -- Down counter
-                if down_reg = MIN_VAL then
-                    down_reg <= MAX_VAL;
+               
+                if down_reg = 0 then
+                    down_reg <= 9;
                 else
                     down_reg <= down_reg - 1;
                 end if;
@@ -47,8 +50,8 @@ begin
         end if;
     end process;
 
-    up_cnt   <= std_logic_vector(up_reg);
-    down_cnt <= std_logic_vector(down_reg);
+   
+    up_cnt   <= std_logic_vector(to_unsigned(up_reg, 4));
+    down_cnt <= std_logic_vector(to_unsigned(down_reg, 4));
 
 end architecture;
-
