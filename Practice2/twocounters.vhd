359b1e209f6e7c2fdc83392bf381cf31e92a1b37
c113106127-sys <c113106127@nkust.edu.tw>
2025-12-11 17:10:06 +0800

    Update twocounters.vhd

--- Practice2/twocounters.vhd
+++ Practice2/twocounters.vhd
@@ -2,76 +2,66 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-
 entity twocounters is
-    Port ( 
-        i_clk        : in  STD_LOGIC; 
-        i_rstp        : in  STD_LOGIC; 
-
-        -- === 計數器 1 的介面 ===
-        i_c1_enable  : in  STD_LOGIC; 
-        i_c1_direction     : in  STD_LOGIC; 
-        i_c1_max     : in  integer range 0 to 255; 
-        i_c1_min     : in  integer range 0 to 255; 
-        o_ledc1     : out STD_LOGIC_VECTOR (7 downto 0); 
-
-        -- === 計數器 2 的介面 ===
-        i_c2_enable  : in  STD_LOGIC; 
-        i_c2_direction     : in  STD_LOGIC; 
-        i_c2_max     : in  integer range 0 to 255; 
-        i_c2_min     : in  integer range 0 to 255; 
-        o_ledc2     : out STD_LOGIC_VECTOR (7 downto 0) 
+    Port (
+        i_clk         : in  STD_LOGIC;
+        i_rstp        : in  STD_LOGIC;
+        i_direction1  : in  STD_LOGIC;  -- counter1 的方向 ('1' = up, '0' = down)
+        i_direction2  : in  STD_LOGIC;  -- counter2 的方向
+        o_ledc1       : out STD_LOGIC_VECTOR (7 downto 0);
+        o_ledc2       : out STD_LOGIC_VECTOR (7 downto 0)
     );
 end twocounters;
+
 architecture Behavioral of twocounters is
 
-    signal r_count1 : integer range 0 to 255 := 0;
-    signal r_count2 : integer range 0 to 255 := 0;
+    constant c_max : integer := 9;
+    constant c_min : integer := 0;
+
+    signal r_count1 : integer range 0 to 9 := 0;
+    signal r_count2 : integer range 0 to 9 := 0;
+
 begin
 
-    -- Process A: 計數器 1
+    -- Counter 1
     process(i_clk, i_rstp)
     begin
         if i_rstp = '1' then
-            r_count1 <= 0; 
+            r_count1 <= c_min;
         elsif rising_edge(i_clk) then
-            if i_c1_enable = '1' then
-                if i_c1_direction = '1' then
-                    if r_count1 < i_c1_max then
-                        r_count1 <= r_count1 + 1; 
-                    else
-                        r_count1 <= i_c1_min;       
-                    end if;
+            if i_direction1 = '1' then  -- up
+                if r_count1 < c_max then
+                    r_count1 <= r_count1 + 1;
                 else
-                    if r_count1 > i_c1_min then
-                        r_count1 <= r_count1 - 1; 
-                    else
-                        r_count1 <= i_c1_max;       
-                    end if;
+                    r_count1 <= c_min;
+                end if;
+            else  -- down
+                if r_count1 > c_min then
+                    r_count1 <= r_count1 - 1;
+                else
+                    r_count1 <= c_max;
                 end if;
             end if;
         end if;
     end process;
 
-    -- Process B: 計數器 2
+    -- Counter 2
     process(i_clk, i_rstp)
     begin
         if i_rstp = '1' then
-            r_count2 <= 0;
+            r_count2 <= c_min;
         elsif rising_edge(i_clk) then
-            if i_c2_enable = '1' then
-                if i_c2_direction = '1' then
-                    if r_count2 < i_c2_max then
-                        r_count2 <= r_count2 + 1;
-                    else
-                        r_count2 <= i_c2_min;
-                    end if;
+            if i_direction2 = '1' then  -- up
+                if r_count2 < c_max then
+                    r_count2 <= r_count2 + 1;
                 else
-                    if r_count2 > i_c2_min then
-                        r_count2 <= r_count2 - 1;
-                    else
-                        r_count2 <= i_c2_max;
-                    end if;
+                    r_count2 <= c_min;
+                end if;
+            else  -- down
+                if r_count2 > c_min then
+                    r_count2 <= r_count2 - 1;
+                else
+                    r_count2 <= c_max;
                 end if;
             end if;
         end if;
