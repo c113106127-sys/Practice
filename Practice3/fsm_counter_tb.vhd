abb8aada5a20aced7498493ae4ab662fa29689ab
c113106127-sys <c113106127@nkust.edu.tw>
2026-01-16 16:10:49 +0800

    Refactor testbench for practice3 component
    Updated testbench for practice3 component, removed unnecessary port declarations and adjusted reset timing.

--- Practice3/fsm_counter_tb.vhd
+++ Practice3/fsm_counter_tb.vhd
@@ -2,44 +2,48 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity tb_practice3 is
--- Testbench 不需要 Port
+
 end tb_practice3;
 
-architecture Behavioral of tb_practice3 is
+architecture behavior of tb_practice3 is 
 
-    COMPONENT practice3
-    GENERIC(
-        DIV_MAX : integer
-    );
-    PORT(
-        i_clk : IN  std_logic;
-        i_rst : IN  std_logic;
-        o_count1 : OUT  std_logic_vector(3 downto 0);
-        o_count2 : OUT  std_logic_vector(3 downto 0)
-    );
-    END COMPONENT;
+  
+    component practice3
+        Generic (
+            DIV_MAX : integer
+        );
+        Port (
+            i_clk    : in STD_LOGIC;
+            i_rst    : in STD_LOGIC;
+            o_count1 : out STD_LOGIC_VECTOR (3 downto 0);
+            o_count2 : out STD_LOGIC_VECTOR (3 downto 0)
+        );
+    end component;
+    
 
-    signal i_clk : std_logic := '0';
-    signal i_rst : std_logic := '0';
+    
+    signal i_clk    : std_logic := '0';
+    signal i_rst    : std_logic := '0';
     signal o_count1 : std_logic_vector(3 downto 0);
     signal o_count2 : std_logic_vector(3 downto 0);
 
+   
     constant clk_period : time := 10 ns;
-
+ 
 begin
-
-    uut: practice3
+ 
+    uut: practice3 
     GENERIC MAP (
+
         DIV_MAX => 4 
     )
     PORT MAP (
-        i_clk => i_clk,
-        i_rst => i_rst,
+        i_clk    => i_clk,
+        i_rst    => i_rst,
         o_count1 => o_count1,
         o_count2 => o_count2
     );
 
- 
     clk_process :process
     begin
         i_clk <= '0';
@@ -47,8 +51,9 @@ begin
         i_clk <= '1';
         wait for clk_period/2;
     end process;
-
  
+
+
     stim_proc: process
     begin		
 
@@ -56,13 +61,11 @@ begin
         wait for 100 ns;	
 
         i_rst <= '1';
-
-
-        wait for 1000 ns; 
         
-        assert false report "Simulation Finished" severity failure;
-        
+   
+        wait for 2000 ns;
+
         wait;
     end process;
 
-end Behavioral;
+end behavior;
