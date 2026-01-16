a2a2fe03792ea01686c2102f5100a9cf42c65bf4
c113106127-sys <c113106127@nkust.edu.tw>
2026-01-16 16:09:53 +0800

    Update fsm_counter.vhd

--- Practice3/fsm_counter.vhd
+++ Practice3/fsm_counter.vhd
@@ -47,82 +47,48 @@ begin
 
     o_count1 <= count1;
     o_count2 <= count2;
-
-    -- ========= FSM  =========
-
+-- ========= FSM  =========
     FSM: process(i_clk, i_rst)
     begin
         if i_rst = '0' then
             state <= S0;
+            count1 <= (others => '0');
+            count2 <= (others => '0');
         elsif rising_edge(i_clk) then
             if clk_en = '1' then 
                 case state is
                     when S0 =>
+                        count2 <= (others => '0'); -- S0 期間 Counter 2 固定為 0
                         if count1 = "1001" then
+                            count1 <= (others => '0'); 
+                            count2 <= "1001";
                             state <= S1;
+                       
+                        else
+                            count1 <= count1 + 1;
+                            count2 <= (others => '0');
                         end if;
-
+                        
+                        
                     when S1 =>
-                        if count2 = "0001" then 
+                        count1 <= (others => '0'); -- S1 期間 Counter 1 固定為 0
+                        if count2 = "0000" then 
+                            count2 <= "1001";
+                        elsif count2 = "0001" then
+                            count2 <= (others => '0');
+                            count1 <= "0001";
                             state <= S0;
+                        else
+                            count2 <= count2 - 1;
+                            count1 <= "0000";
                         end if;
-                    when others =>
-                        null;
+                        
+    
                 end case;
             end if;
         end if;
     end process;
 
-   -- ========= Counter 1  =========
-    counter1: process(i_clk, i_rst)
-    begin
-        if i_rst = '0' then
-            count1 <= "0000";
-        elsif rising_edge(i_clk) then
-            if clk_en = '1' then 
-  
-                if state = S0 then
-                    if count1 = "1001" then
-                        count1 <= "0000"; 
-                    else
-                        count1 <= count1 + 1;
-                    end if;
-                
-
-                elsif state = S1 and count2 = "0001" then
-                    count1 <= "0001"; 
-
-                else
-                    count1 <= "0000";
-                end if;
-            end if;
-        end if;
-    end process;
-
-    -- ========= Counter 2 =========
-    counter2: process(i_clk, i_rst)
-    begin
-        if i_rst = '0' then
-            count2 <= "0000"; 
-        elsif rising_edge(i_clk) then
-            if clk_en = '1' then 
-
-                if state = S1 then
-                    if count2 = "0000" then
-                        count2 <= "0000"; 
-                    else
-                        count2 <= count2 - 1;
-                    end if;
-
-
-                elsif state = S0 and count1 = "1001" then
-                    count2 <= "1001"; 
-
-                else
-                    count2 <= "0000";
-                end if;
-            end if;
-        end if;
-    end process;
+ 
 
 end Behavioral;
