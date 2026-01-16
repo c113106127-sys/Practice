b3d135bef172795180e3b7982596b824983030b2
c113106127-sys <c113106127@nkust.edu.tw>
2026-01-16 11:01:53 +0800

    Update fsm_counter.vhd

--- Practice3/fsm_counter.vhd
+++ Practice3/fsm_counter.vhd
@@ -4,6 +4,9 @@ use IEEE.numeric_std.all;
 use IEEE.std_logic_unsigned.all;
 
 entity practice3 is
+    Generic (
+        DIV_MAX : integer := 49999999 
+    );
     Port (
         i_clk    : in STD_LOGIC;
         i_rst    : in STD_LOGIC;
@@ -20,92 +23,105 @@ architecture Behavioral of practice3 is
     type FSM_state is (S0, S1);
     signal state : FSM_state;
 
-    -- ========= 除頻器宣告 =========
-    signal div_cnt : integer range 0 to 49999999;
-    signal clk_div : STD_LOGIC;
+    signal div_cnt : integer range 0 to DIV_MAX; 
+    signal clk_en  : std_logic;                  
 
 begin
-    o_count1 <= count1;
-    o_count2 <= count2;
 
-    -- ========= 除頻器 (50MHz → 1Hz) =========
-    clk_divider : process(i_clk, i_rst)
+    -- ========= 除頻器 =========
+    CLK_DIVIDER_PROC: process(i_clk, i_rst)
     begin
         if i_rst = '0' then
             div_cnt <= 0;
-            clk_div <= '0';
+            clk_en  <= '0';
         elsif rising_edge(i_clk) then
-            if div_cnt = 49999999 then
+            if div_cnt = DIV_MAX then
                 div_cnt <= 0;
-                clk_div <= not clk_div;
+                clk_en  <= '1'; 
             else
                 div_cnt <= div_cnt + 1;
+                clk_en  <= '0'; 
             end if;
         end if;
     end process;
 
-    -- ========= FSM =========
-    FSM: process(clk_div, i_rst)
+    o_count1 <= count1;
+    o_count2 <= count2;
+
+    -- ========= FSM  =========
+
+    FSM: process(i_clk, i_rst)
     begin
         if i_rst = '0' then
             state <= S0;
-        elsif rising_edge(clk_div) then
-            case state is
-                when S0 =>
-                    if count1 = "1001" then
-                        state <= S1;
-                    end if;
+        elsif rising_edge(i_clk) then
+            if clk_en = '1' then 
+                case state is
+                    when S0 =>
+                        if count1 = "1001" then
+                            state <= S1;
+                        end if;
 
-                when S1 =>
-                    if count2 = "1001" then
-                        state <= S0;
-                    end if;
-
-                when others =>
-                    null;
-            end case;
+                    when S1 =>
+                        if count2 = "0001" then 
+                            state <= S0;
+                        end if;
+                    when others =>
+                        null;
+                end case;
+            end if;
         end if;
     end process;
 
-    -- ========= Counter 1 =========
-    counter1: process(clk_div, i_rst)
+   -- ========= Counter 1  =========
+    counter1: process(i_clk, i_rst)
     begin
         if i_rst = '0' then
             count1 <= "0000";
-        elsif rising_edge(clk_div) then
-            case state is
-                when S0 =>
-                    if count1 < "1001" then
+        elsif rising_edge(i_clk) then
+            if clk_en = '1' then 
+  
+                if state = S0 then
+                    if count1 = "1001" then
+                        count1 <= "0000"; 
+                    else
                         count1 <= count1 + 1;
                     end if;
+                
 
-                when S1 =>
+                elsif state = S1 and count2 = "0001" then
+                    count1 <= "0001"; 
+
+                else
                     count1 <= "0000";
-
-                when others =>
-                    null;
-            end case;
+                end if;
+            end if;
         end if;
     end process;
 
     -- ========= Counter 2 =========
-    counter2: process(clk_div, i_rst)
+    counter2: process(i_clk, i_rst)
     begin
         if i_rst = '0' then
-            count2 <= "0000";
-        elsif rising_edge(clk_div) then
-            case state is
-                when S0 =>
-                    count2 <= "0000";
+            count2 <= "0000"; 
+        elsif rising_edge(i_clk) then
+            if clk_en = '1' then 
 
-                when S1 =>
-                    if count2 < "1001" then
-                        count2 <= count2 + 1;
+                if state = S1 then
+                    if count2 = "0000" then
+                        count2 <= "0000"; 
+                    else
+                        count2 <= count2 - 1;
                     end if;
 
-                when others =>
-                    null;
-            end case;
+
+                elsif state = S0 and count1 = "1001" then
+                    count2 <= "1001"; 
+
+                else
+                    count2 <= "0000";
+                end if;
+            end if;
         end if;
     end process;
 
